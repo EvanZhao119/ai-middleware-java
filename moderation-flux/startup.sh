1b1b3078b08d1b2a44aba6218a37d641238d491c
zhaohongnan <evanzhao119@gmail.com>
2026-01-30 04:47:55 +0800

    flux startup

--- moderation-flux/startup.sh
+++ moderation-flux/startup.sh
@@ -1,1 +1,1 @@
-nohup java -Xms24m -Xmx80m -XX:+UseSerialGC -XX:MaxMetaspaceSize=48m -Xss256k -XX:ReservedCodeCacheSize=24m -jar ./target/moderation-flux-1.0-SNAPSHOT.jar &
+nohup java -Xms24m -Xmx80m -XX:+UseSerialGC -XX:MaxMetaspaceSize=128m -Xss256k -XX:ReservedCodeCacheSize=24m -jar ./target/moderation-flux-1.0-SNAPSHOT.jar &
